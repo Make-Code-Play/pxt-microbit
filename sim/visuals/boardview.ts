60478edd3cd98ec9663098c1ecdb0d2d37af919e
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2018-11-01 13:32:34 +0100

    Add files via upload

--- sim/visuals/boardview.ts
+++ sim/visuals/boardview.ts
@@ -2,9 +2,9 @@ namespace pxsim.visuals {
     mkBoardView = (opts: BoardViewOptions): BoardView => {
         return new visuals.MicrobitBoardSvg({
             runtime: runtime,
-            theme: visuals.randomTheme(opts.highContrast),
+            theme: visuals.randomTheme(),
             disableTilt: false,
-            wireframe: opts.wireframe
+            wireframe: opts.wireframe,
         });
     }
 }
